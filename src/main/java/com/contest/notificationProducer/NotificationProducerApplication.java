efe8c773fc1cbc9cc7a7d5a15dd358f4c4df4698
sahilkothari <sahilkothari114@gmail.com>
2019-01-29 21:17:59 +0530

    Initial Commit

---
+++ src/main/java/com/contest/notificationProducer/NotificationProducerApplication.java
@@ -0,0 +1,14 @@
+package com.contest.notificationProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationProducerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/contest/notificationProducer/NotificationProducerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.contest.notificationProducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotificationProducerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
